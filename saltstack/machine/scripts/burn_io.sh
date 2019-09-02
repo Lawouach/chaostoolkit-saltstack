25d7fd47e9c81a82dad2d1f009409b0fffb034c5
Liu, Xiaopeng (133) <xiaopeng.liu@daimler.com>
2019-09-02 16:09:50 +0800

    use sh will result

--- saltstack/machine/scripts/burn_io.sh
+++ saltstack/machine/scripts/burn_io.sh
@@ -9,5 +9,18 @@ EOF
 
 chmod +x /tmp/loop.sh
 timeout --preserve-status $duration /tmp/loop.sh
+
+# while true;
+# do
+#     dd if=/dev/urandom of=/experiment_burnio bs=1M count=1024 iflag=fullblock status=none
+# done &
+
+ret=$?
+if [ $ret -eq 0 ]; then
+    echo "experiment burnio -> <$instance_id>: success"
+else
+    echo "experiment brunio -> <$instance_id>: fail"
+fi
+
 sudo rm /root/burn
 sudo rm /tmp/loop.sh
